affc85b7ea8b90416f715c2858edfe9f8f8d12eb
Liam <debli620@student.otago.ac.nz>
2022-08-09 21:17:55 +1200

    Remove unused files

--- Cuisinele/app/src/main/java/com/example/cuisinele/Cuisinele.kt
+++ Cuisinele/app/src/main/java/com/example/cuisinele/Cuisinele.kt
@@ -5,14 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.cuisinele.databinding.FragmentFirstBinding
+import com.example.cuisinele.databinding.CuisineleBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class Cuisinele : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: CuisineleBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,7 +22,7 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = CuisineleBinding.inflate(inflater, container, false)
         setKeyButtons();
 
         return binding.root
