d9cbb2c251ea0914a44b4ff7c747c421e7afaddb
Jacob Rowe <rowja703@student.otago.ac.nz>
2022-08-19 14:20:58 +1200

    testing documentation

--- Cuisinele/app/src/main/java/com/example/cuisinele/Cuisinele.kt
+++ Cuisinele/app/src/main/java/com/example/cuisinele/Cuisinele.kt
@@ -233,7 +233,7 @@ class Cuisinele : Fragment() {
     /**
      * Method sets up the show/hide guess button.
      */
-    private fun toggleGuesses() {
+    fun toggleGuesses() {
         binding.displayGuessButton.setOnClickListener {
             if (binding.guessDisplay.visibility == View.INVISIBLE) {
                 binding.guessDisplay.visibility = View.VISIBLE
