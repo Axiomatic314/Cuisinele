b6048b07cc8fb7c082c3f8edb9b60c517d680b2c
Jacob Rowe <rowja703@student.otago.ac.nz>
2022-09-19 21:03:47 +1200

    a rough success page guess and answer display

--- Cuisinele/app/src/main/java/com/example/cuisinele/Success.kt
+++ Cuisinele/app/src/main/java/com/example/cuisinele/Success.kt
@@ -7,8 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.cuisinele.data.ContextApplication
+import com.example.cuisinele.data.CuisineleDAO
+import com.example.cuisinele.data.CuisineleDB
+import com.example.cuisinele.data.models.Country
+import com.example.cuisinele.data.models.Dish
+import com.example.cuisinele.data.models.Hint
 import com.example.cuisinele.databinding.SuccessPageBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.lang.String
+import java.time.LocalDate
 import java.time.LocalTime
 import java.util.concurrent.TimeUnit
 
@@ -22,6 +32,10 @@ class Success : Fragment() {
     //This property is only valid between onCreateView and onDestroyView
     private val binding get() = _binding!!
     private lateinit var timer : CountDownTimer
+    private lateinit var dao: CuisineleDAO
+    private var dish: Dish? = null
+    private var hints: List<Hint>? = null
+    private var country: Country? = null
 
     /**
      * Method creates and returns the view hierarchy associated with this fragment and inflates the page to be viewed.
@@ -35,6 +49,7 @@ class Success : Fragment() {
     ): View {
         MainActivity.canGoBack = false
         _binding = SuccessPageBinding.inflate(inflater, container, false)
+        getData()
         setCountDown()
         setContinue()
         return binding.root
@@ -89,4 +104,52 @@ class Success : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun getData() {
+        GlobalScope.launch(Dispatchers.IO) {
+            dao = CuisineleDB.getInstance(ContextApplication.applicationContext()).cuisineleDAO()
+            if (Settings.dailyGames) {
+                // Convert current time since linux epoch from milliseconds to days
+                var currentDate = LocalDate.now().toEpochDay()
+                // The date we choose the dishes to start cycling from
+                var cycleStartDate = Settings.startDate.toEpochDay()
+                if (currentDate > cycleStartDate) {
+                    // calculate the day since the dish cycle begun and use modulus of the number of dishes to allow recycling of dishes
+                    var dishID: Int = ((currentDate - cycleStartDate) % dao.getDishes().size).toInt()
+                    dish = dao.getDishByID(dishID)
+                } else {
+                    // TODO: add message/exception for when the dish cycle hasn't begun (this should never occur)
+                }
+            } else {
+                var allDishes = dao.getDishes()
+                dish = (allDishes.filter { x -> !x.IsComplete }).first()
+            }
+
+            if (dish != null) {
+                country = dao.getCountryByID(dish!!.CountryID)
+                hints = dao.getHintsByDishID(dish!!.DishID)
+                binding.correctAnswer.text = country!!.CountryName
+
+                if (dish!!.GuessOne != 0) {
+                    binding.guess1TextView.text = dao.getCountryByID(dish!!.GuessOne)!!.CountryName
+                }
+                if (dish!!.GuessTwo != 0) {
+                    binding.guess2TextView.text = dao.getCountryByID(dish!!.GuessTwo)!!.CountryName
+                }
+                if (dish!!.GuessThree != 0) {
+                    binding.guess3TextView.text = dao.getCountryByID(dish!!.GuessThree)!!.CountryName
+                }
+                if (dish!!.GuessFour != 0) {
+                    binding.guess4TextView.text = dao.getCountryByID(dish!!.GuessFour)!!.CountryName
+                }
+                if (dish!!.GuessFive != 0) {
+                    binding.guess5TextView.text = dao.getCountryByID(dish!!.GuessFive)!!.CountryName
+                }
+                if (dish!!.GuessSix != 0) {
+                    binding.guess6TextView.text = dao.getCountryByID(dish!!.GuessSix)!!.CountryName
+                }
+            }
+        }
+    }
+
 }
